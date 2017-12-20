fe96a7fed920f2f365a655c024f3a042e18b054f
William Kwao <williamkwao@Williams-MacBook-Pro-2.local>
2017-12-19 21:45:58 -0500

    card and navbar component file

--- src/index.js
+++ src/index.js
@@ -3,7 +3,8 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import store from './store';
 import { BrowserRouter, Route } from 'react-router-dom';
-import NavBar from './NavBar';
+import NavBar from './components/NavBar';
+import Card from './components/Card';
 import './index.css';
 
 class SearchBar extends React.Component {
@@ -14,39 +15,6 @@ class SearchBar extends React.Component {
             </div>
         );
     };
-}
-class Cards extends React.Component {
-    render() {
-        return (
-            <div className="row cards">
-                <div className="col s12 m3 l3">
-                    <div className="card item-card amz">
-                        <div className="card-image waves-effect waves-block waves-light">
-                            <img className="activator item-image" src="img/iphone2.jpg" />
-                        </div>
-                        <div className="card-content">
-                            <div className="card-title border">
-                                <span className="activator item-title">Straight Talk Apple iPhone 7 32GB Prepaid Smartphone, Rose Gold.</span>
-                            </div>
-                            <div className="card-price">
-                                <p>Price: <em>$449.00</em></p>
-                            </div>
-                            <div className="card-link">
-                                <p><a href="#">Go to Amazon</a></p>
-                            </div>
-                            <div className="retail-logo">
-                                <img src="img/amazonlogo.png" />
-                            </div>
-                        </div>
-                        <div className="card-reveal">
-                            <span className="card-title grey-text text-darken-4">More Info<i class="material-icons right">close</i></span>
-                            <p>Straight Talk Apple iPhone 7 32GB Prepaid Smartphone, Rose Gold.</p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        )
-    }
 }
 
 class Banner extends React.Component {
@@ -88,7 +56,7 @@ class PrimaryLayout extends React.Component {
             <div className="canvas">
                 <NavBar />
                 <Banner />
-                <Cards />
+                <Card />
 
             </div>
 
