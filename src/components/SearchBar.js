4273bfc96d704c1c3b597e90c30caae21ab817ff
William Kwao <williamkwao@Williams-MacBook-Pro-2.local>
2017-12-28 23:25:07 -0500

    added state and added submit on enter to searchbar

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,15 +1,38 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 
 export default class SearchBar extends Component {
+    
+    state = {
+        searchTerm: '',
+    };
+
+    onSearchChange = (e) => {
+        const search = e.target.value;
+        this.setState({ searchTerm: search });
+        console.log(this.state.searchTerm);
+
+    };
+
+    onSearchSubmit = (e) => {
+        if (e) e.preventDefault();
+        const search = e.target.value;
+        this.setState({ searchTerm: '' });
+        console.log(this.state.searchTerm);
+    }
+
     render() {
-       
+
         return (
             <div id="seach-bar" className="input-field col s6 m4 l4">
-                <input
-                    placeholder="Find the best price ..."
-                    id="search" type="search"
-                    className="validate"
-                />
+                <form onSubmit={this.onSearchSubmit}>
+                    <input
+                        placeholder="Find the best price ..."
+                        id="search" type="search"
+                        className="validate"
+                        onChange={this.onSearchChange}
+                        value={this.state.searchTerm}
+                    />
+                </form>
             </div>
         );
     };
