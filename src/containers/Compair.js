81bb1c7dedd0ed85a56d3e29d72fb17fbeff8e08
esayas12 <esayas.andom@yahoo.com>
2017-12-29 11:26:07 -0500

    For Invalid URL added page not found

--- src/containers/Compair.js
+++ src/containers/Compair.js
@@ -3,17 +3,20 @@ import ReactDOM from 'react-dom';
 import PropTypes from 'prop-types'
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route,Switch} from 'react-router-dom';
 import NavBar from '../components/NavBar';
 import LandingPage from './LandingPage'
+import notFound from '../components/notFound'
 import SearchResultLayout from './SearchResultLayout';
 import * as SearchActionCreators from '../actions/search'
+
 
 class PrimaryLayout extends React.Component {
     render() {
         return (
             <div className="canvas">
                 <NavBar />
+                <Switch>
                 <Route path="/" exact component={LandingPage} />
                 <Route
                     path="/search"
@@ -24,6 +27,8 @@ class PrimaryLayout extends React.Component {
                         />
                     )}
                 />
+                <Route component={notFound}/>
+                </Switch>
             </div>
 
         );
