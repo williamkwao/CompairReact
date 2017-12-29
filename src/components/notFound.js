16abc78c3211d5bbd347c14558c43cb677a660bd
esayas12 <esayas.andom@yahoo.com>
2017-12-29 11:26:07 -0500

    For Invalid URL added page not found

---
+++ src/components/notFound.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+class notFound extends React.Component{
+    render(){
+        return(
+            <h2 className="notFound"> Page not found </h2>
+            
+        )
+        
+        
+    }
+}
+
+export default notFound;
