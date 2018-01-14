bc96b2401d7d40d414c216a24c8b540a7f2fe5ac
William Kwao <williamkwao@gmail.com>
2018-01-14 18:10:47 -0500

    switched from jsonp to axios and now calling compair rest api

--- src/containers/Compair.js
+++ src/containers/Compair.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types'
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import { BrowserRouter, Route } from 'react-router-dom';
+import axios from 'axios'
 import jsonp from 'jsonp';
 import NavBar from '../components/NavBar';
 import LandingPage from './LandingPage'
@@ -17,22 +18,17 @@ class Compair extends React.Component {
     }
 
     fetchAndUpdate = (searchTerm) => {
-        const { dispatch} = this.props;
+        const { dispatch } = this.props;
         const getSearchResults = bindActionCreators(SearchActionCreators.getSearchResults, dispatch);
-        jsonp(ApiProperties.WMT_API + searchTerm, null, function (err, data) {
-            if (err) {
-                console.error(err.message);
-            } 
-            else {
-                console.log(data);
-                if (data.totalResults > 0){
-                    getSearchResults(searchTerm, data.items);
-                }else{
-                    getSearchResults(searchTerm, []); 
-                }
-                
-            }
-        });
+        axios.get(ApiProperties.WMT_API + searchTerm)
+            .then((response) => {
+                console.log(response);
+                getSearchResults(searchTerm, response.data.items);
+            })
+            .catch((error) => {
+                console.log(error)
+                getSearchResults(searchTerm, []);
+            });
     }
 
     render() {
@@ -52,10 +48,9 @@ class PrimaryLayout extends React.Component {
         let canvasstyle = "canvas";
         if (window.location.pathname.startsWith("/search")) {
             canvasstyle += " searchcanvas";
-            //searchcanvas = { "backgroundImage": "none" };
         }
         return (
-            <div className= {canvasstyle}>
+            <div className={canvasstyle}>
                 <NavBar />
 
                 <Route
@@ -78,7 +73,7 @@ class PrimaryLayout extends React.Component {
                         />
                     )}
                 />
-              
+
             </div>
 
         );
